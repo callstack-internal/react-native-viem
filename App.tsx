c7526e38d9d6d07ac029cb0ae0c701bf83d1e5e2
Thiago Brezinski <thiagobrez@gmail.com>
2023-10-10 10:41:58 +0100

    feat(viem): install viem and get public data

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
+import './polyfills';
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import {createPublicClient, formatEther, http} from 'viem';
+import {mainnet} from 'viem/chains';
+
+type Data = {
+  blockNumber: bigint;
+  gasPrice: bigint;
+};
+
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
 
 export default function App() {
+  const [publicData, setPublicData] = useState<Data>({
+    blockNumber: BigInt(0),
+    gasPrice: BigInt(0),
+  });
+
+  useEffect(() => {
+    const getPublicData = async () => {
+      const [blockNumber, gasPrice] = await Promise.all([
+        publicClient.getBlockNumber(),
+        publicClient.getGasPrice(),
+      ]);
+
+      setPublicData({blockNumber, gasPrice});
+    };
+
+    void getPublicData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Block number: {String(publicData.blockNumber)}</Text>
+      <Text>Gas price: {formatEther(publicData.gasPrice)} ETH</Text>
     </View>
   );
 }
