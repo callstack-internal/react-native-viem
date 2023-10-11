ca1214c05096e4aee4c3f384c8e9ba1b2df8d0d9
Thiago Brezinski <thiagobrez@gmail.com>
2023-10-11 16:35:24 +0100

    feat(metamask): refactor page

--- App.tsx
+++ App.tsx
@@ -20,5 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: 20,
   },
 });
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,33 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
-import {formatEther} from 'viem';
-import {goerli, mainnet} from 'viem/chains';
+import {Address, Chain, formatEther, hexToNumber} from 'viem';
+import {mainnet, sepolia} from 'viem/chains';
 import {publicClient} from '../clients/public';
 import {useWalletClient} from '../contexts/WalletClientContext';
 
-type NetworkData = {
-  blockNumber: bigint;
-  gasPrice: bigint;
-};
-
-type WalletData = {
-  address: `0x${string}`;
-  balance: bigint;
-  chainId: number;
-};
-
-export const CHAINS: {[key: number]: string} = {
-  1: 'Mainnet',
-  5: 'Goerli',
-} as const;
+export const CHAINS = [mainnet, sepolia];
 
 export default function HomePage() {
   const {provider, walletClient} = useWalletClient();
-  const [networkData, setNetworkData] = useState<NetworkData>({
-    blockNumber: BigInt(0),
-    gasPrice: BigInt(0),
-  });
-  const [walletData, setWalletData] = useState<WalletData>();
+  const [blockNumber, setBlockNumber] = useState(BigInt(0));
+  const [gasPrice, setGasPrice] = useState(BigInt(0));
+  const [isConnected, setIsConnected] = useState(false);
+  const [chain, setChain] = useState<Chain>(CHAINS[0]);
+  const [address, setAddress] = useState<Address>('0x0000');
+  const [balance, setBalance] = useState(BigInt(0));
+  const filteredChains = CHAINS.filter(item => item.id !== chain.id);
 
   useEffect(() => {
     const getNetworkData = async () => {
@@ -36,64 +24,83 @@ export default function HomePage() {
         publicClient.getGasPrice(),
       ]);
 
-      setNetworkData({blockNumber, gasPrice});
+      setBlockNumber(blockNumber);
+      setGasPrice(gasPrice);
     };
 
     void getNetworkData();
   }, []);
 
-  const onConnect = async () => {
-    const [address] = await walletClient.requestAddresses();
-    const balance = await publicClient.getBalance({address});
-    const chainId = await walletClient.getChainId();
+  useEffect(() => {
+    const onChainChangedEvent = (arg: unknown) => {
+      const data = arg as `0x${string}`;
+      const chainId = hexToNumber(data);
+      const chain = CHAINS.find(chain => chain.id === chainId) ?? CHAINS[0];
+      setChain(chain);
+    };
 
-    setWalletData({address, balance, chainId});
-  };
+    const onConnectEvent = async (arg: unknown) => {
+      const data = arg as {chainId: `0x${string}`};
+      onChainChangedEvent(data.chainId);
 
-  const onDisconnect = () => {
-    provider.handleDisconnect({terminate: true});
-    setWalletData(undefined);
-  };
+      const [address] = await walletClient.getAddresses();
+      const balance = await publicClient.getBalance({address});
 
-  const onSwitchChain = async (chainId: number) => {
-    await walletClient.switchChain({id: chainId});
-    const newChainId = await walletClient.getChainId();
+      setAddress(address);
+      setBalance(balance);
+      setIsConnected(true);
+    };
 
-    if (walletData) {
-      setWalletData({...walletData, chainId: newChainId});
-    }
-  };
+    const onDisconnectEvent = () => {
+      setIsConnected(false);
+    };
+
+    provider.on('chainChanged', onChainChangedEvent);
+    provider.on('connect', onConnectEvent);
+    provider.on('disconnect', onDisconnectEvent);
+
+    return () => {
+      provider.removeListener('chainChanged', onChainChangedEvent);
+      provider.removeListener('connect', onConnectEvent);
+      provider.removeListener('disconnect', onDisconnectEvent);
+    };
+  }, [provider, walletClient]);
 
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1}>
-        Block number: {String(networkData.blockNumber)}
-      </Text>
-      <Text numberOfLines={1}>
-        Gas price: {formatEther(networkData.gasPrice)} ETH
-      </Text>
+      <Text numberOfLines={1}>Block number: {String(blockNumber)}</Text>
+      <Text numberOfLines={1}>Gas price: {formatEther(gasPrice)} ETH</Text>
 
-      {walletData && (
+      {isConnected && (
         <View style={styles.block}>
-          <Text numberOfLines={1}>Address: {walletData.address}</Text>
-          <Text numberOfLines={1}>Chain: {CHAINS[walletData.chainId]}</Text>
-          <Text numberOfLines={1}>
-            Balance: {formatEther(walletData.balance)} ETH
-          </Text>
+          <Text numberOfLines={1}>Address: {address}</Text>
+          <Text numberOfLines={1}>Balance: {formatEther(balance)} ETH</Text>
         </View>
       )}
 
       <View style={styles.block}>
-        <Button title="Connect" onPress={onConnect} />
-        <Button
-          title="Switch to Ethereum"
-          onPress={() => onSwitchChain(mainnet.id)}
-        />
-        <Button
-          title="Switch to Goerli"
-          onPress={() => onSwitchChain(goerli.id)}
-        />
-        <Button title="Disconnect" onPress={onDisconnect} />
+        {isConnected ? (
+          <>
+            <Text style={styles.chainText}>Connected to: {chain.name}</Text>
+            {filteredChains.map(chain => (
+              <Button
+                key={chain.id}
+                title={`Switch to ${chain.name}`}
+                onPress={() => walletClient.switchChain({id: chain.id})}
+              />
+            ))}
+            <Button
+              title="Disconnect"
+              onPress={() => provider.handleDisconnect({terminate: true})}
+              color="red"
+            />
+          </>
+        ) : (
+          <Button
+            title="Connect"
+            onPress={() => walletClient.requestAddresses()}
+          />
+        )}
       </View>
     </View>
   );
@@ -102,10 +109,12 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
   },
   block: {
     marginTop: 32,
+  },
+  chainText: {
+    textAlign: 'center',
   },
 });
